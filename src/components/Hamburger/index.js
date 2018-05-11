e505ce44e36632924d6ef45100d22f9bc49fd7d0
Prathmesh Desai <d.prathmesh@gmail.com>
2018-05-10 22:30:18 -0400

    fix hamburger menu to animate & maintain position on click

--- src/components/Hamburger/index.js
+++ src/components/Hamburger/index.js
@@ -6,20 +6,9 @@ import * as Styled from './styled';
 export default class Hamburger extends Component {
   render() {
     return (
-      <Styled.Div onClick={() => this.props.toggleDrawer()}>
-        <Styled.Line
-          transform={this.props.showDrawer && 'translateY(0.3em) rotate(45deg)'}
-          margin={!this.props.showDrawer ? '0 0 0.3em 0' : '0.5em'}
-        />
-        <Styled.Line
-          hide={this.props.showDrawer}
-          margin="0 0 .3em 0"
-        />
-        <Styled.Line
-          transform={this.props.showDrawer && 'translateY(-0.3em) rotate(-45deg)'}
-          margin={!this.props.showDrawer ? '0 0 0.3em 0' : '0.5em'}
-        />
-      </Styled.Div>
+      <Styled.HamburgerWrapper onClick={() => this.props.toggleDrawer()} className="menu-wrapper">
+        <Styled.HamburgerMenu animate={this.props.showDrawer} />
+      </Styled.HamburgerWrapper>
     );
   }
 }
--- src/components/Hamburger/styled.js
+++ src/components/Hamburger/styled.js
@@ -1,17 +1,59 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Div = styled.div`
+export const HamburgerWrapper = styled.div`
   cursor: pointer;
   position: absolute;
-  top: 20px;
-  right: 20px;
+  top: 40px;
+  right: 40px;
+  width: 30px;
+
+  height: 16px;
   z-index: 2;
 `;
+export const HamburgerMenu = styled.div`
+  position: relative;
+  transform: translateY(5px);
+  background: #979797;
+  transition: all 0ms 300ms;
 
-export const Line = styled.hr`
   width: 30px;
-  border: solid 1px #979797;
-  margin: ${(props) => props.margin};
-  transform: ${(props) => props.transform ? props.transform : 'none'};
-  display: ${(props) => props.hide && 'none'}
+  height: 2px;
+
+  ${(props) => (props.animate && css`
+    background: rgba(255, 255, 255, 0);
+  `)}
+
+  &:before {
+    width: 30px;
+    height: 2px;
+    content: "";
+    position: absolute;
+    left: 0;
+    bottom: 7px;
+    background: #979797;
+    transition: bottom 300ms 300ms cubic-bezier(0.23, 1, 0.32, 1), transform 300ms cubic-bezier(0.23, 1, 0.32, 1);
+
+    ${(props) => (props.animate && css`
+      bottom: 0;
+      transform: rotate(-45deg);
+      transition: bottom 300ms cubic-bezier(0.23, 1, 0.32, 1), transform 300ms 300ms cubic-bezier(0.23, 1, 0.32, 1);
+    `)}
+  }
+
+  &:after {
+    width: 30px;
+    height: 2px;
+    content: "";
+    position: absolute;
+    left: 0;
+    top: 7px;
+    background: #979797;
+    transition: top 300ms 300ms cubic-bezier(0.23, 1, 0.32, 1), transform 300ms cubic-bezier(0.23, 1, 0.32, 1);
+
+    ${(props) => (props.animate && css`
+      top: 0;
+      transform: rotate(45deg);
+      transition: top 300ms cubic-bezier(0.23, 1, 0.32, 1), transform 300ms 300ms cubic-bezier(0.23, 1, 0.32, 1);
+    `)}
+  }
 `;
